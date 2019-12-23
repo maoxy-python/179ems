8a6291ce58ee622a92c7c4c57e11550456e8a54b
maoxy-python <maoxy@zparkhr.com.cn>
2019-12-23 16:04:58 +0800

    在dev的demo文件中新增了demo视图

--- utils/demo.py
+++ utils/demo.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
+
+
 def index(request):
 
     print(123)
+    return HttpResponse()
+
+
+def demo(request):
+
+    print("我是在dev新增的demo视图")
+
+    return HttpResponse()
